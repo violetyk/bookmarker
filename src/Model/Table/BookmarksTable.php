2e9df389d396e69f92fa352d90a4a96334aea8d6
violetyk <yuhei.kagaya@gmail.com>
2015-05-01 18:48:15 +0900

    Finish Bookmarker Tutorial
    http://book.cakephp.org/3.0/en/tutorials-and-examples/bookmarks/intro.html

---
+++ src/Model/Table/BookmarksTable.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Bookmark;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Bookmarks Model
+ */
+class BookmarksTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $this->table('bookmarks');
+        $this->displayField('title');
+        $this->primaryKey('id');
+        $this->addBehavior('Timestamp');
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsToMany('Tags', [
+            'foreignKey' => 'bookmark_id',
+            'targetForeignKey' => 'tag_id',
+            'joinTable' => 'bookmarks_tags'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create')
+            ->allowEmpty('title')
+            ->allowEmpty('description')
+            ->allowEmpty('url');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+        return $rules;
+    }
+
+    public function findTagged(Query $query, array $options)
+    {
+      $fields = [
+        'Bookmarks.id',
+        'Bookmarks.title',
+        'Bookmarks.url',
+      ];
+
+      return $this->find()
+        ->distinct($fields)
+        ->matching('Tags', function ($q) use ($options) {
+          return $q->where(['Tags.title IN' => $options['tags']]);
+        });
+    }
+}
